6c9c64b220f1926da50da9eb5e7f77c61ba0d48e
Daniel Gilardoni <dany.gilardoni@gmail.com>
2022-04-28 13:09:35 +0200

    projet.c

--- projet.c
+++ projet.c
@@ -74,12 +74,41 @@ unbounded_int string2unbounded_int(const char *e){
     return u;
 }
 
+unbounded_int ll2unbounded_int(long long i){//Sinon transformer en unbounded directe
+    int a = i;
+    int b = a%10;
+    int count = 0;
+    do{
+        count++;
+        a= a/10;
+    }while(a>10);
+    char *long_s = malloc(sizeof(char)*count);
+    if(long_s == NULL){
+        unbounded_int u;
+        u.signe = '*';
+        return u;
+    }
+    long_s += count-1;
+    a = i;
+    for(;count>0;count--){
+        *long_s = (char)b;
+        a = a/10;
+        b = a%10;
+        if(a != 0)
+            long_s--;//Sinon à la derniére iteration de la boucle on revient trop en arriére je crois
+    }
+    return string2unbounded_int(long_s);
+}
+
 
 int main(){
     unbounded_int u = string2unbounded_int("-125");
     unbounded_int ui = string2unbounded_int("az-125");
+    long long i = 102568;
+    unbounded_int ul = ll2unbounded_int(i);
     free_unbouded_int(u);
     free_unbouded_int(ui);
+    free_unbouded_int(ul);
 
     return 0;
 }
